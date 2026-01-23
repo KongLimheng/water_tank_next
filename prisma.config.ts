49f93f791dd2346cb7ebbf9c7da1c7b8e90892a7
KongLimheng <banakama12@gmail.com>
2026-01-24 02:36:48 +0700

    db seed setting

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { defineConfig, env } from 'prisma/config'
-
+config()
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
