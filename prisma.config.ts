8cabae1f147949468c37fa1c8bbd86350ae3c03a
KongLimheng <banakama12@gmail.com>
2026-01-23 23:21:06 +0700

    prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config'
 import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
+    //seed: 'tsx ./server/lib/seed.ts',
   },
 
   datasource: {
